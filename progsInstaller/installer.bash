1f590643c272ea5dc3e6dee2d0c02f8f2ed3b08a
PavelRudenko <solmessier@gmail.com>
2021-11-25 02:25:29 +0200

    code cleaning and adding some vars

--- progsInstaller/installer.bash
+++ progsInstaller/installer.bash
@@ -1,30 +1,56 @@
 #!/bin/bash
 
+############################## COLORS ######################################################
+titlecol="\e[36m"
+titlecol2="\e[91m"
+titlecol3="\e[93m"
+############################################################################################
+
+############################################################################################
+## If you want to change exiting key change it in the line below. buttonKey = YOUR BUTTON  #
+############################################################################################
+buttonKey="q"
+
 ## Picture that looking good =)
-printf '\e[1;34m%-6s\e[m\n' "########################################################################"
-printf '\e[1;34m%-6s\e[m\n' "#                                                                      #"
-printf '\e[1;34m%-6s\e[m\n' "#$(printf '\e[1;31m%-6s\e[m' "                        INSTALLER")$(printf '\e[1;34m%-6s\e[m\n' "                                     #")"
-printf '\e[1;34m%-6s\e[m\n' "#                                                                      #"
-printf '\e[1;34m%-6s\e[m\n' "########################################################################"
+printf "%b#######################################################################\n" "$titlecol"
+printf "%b#                                                                     #\n" "$titlecol"
+printf "%b#""$titlecol"
+printf "%b                             INSTALLER                               %b#\n" "$titlecol2" "$titlecol"
+printf "%b#                                                                     #\n" "$titlecol"
+printf "%b#######################################################################\n\n" "$titlecol"
 
 programTerminator(){ # This funtcion drop off from program by pressing specific key
 read -r -t 0.5 -n 1 -s holder && keystroke="$holder"
-############################################################################################
-## If you want to change exiting key change it in the line below. $keystroke = YOUR BUTTON #
-############################################################################################
-    if [[ $keystroke = q ]] ; then
-            printf "\e[1;34m%s\n" "Quitting from the program"
-            printf "\e[1;34m%s\n" "Thank You for using program!"
+    if [[ $keystroke = "$buttonKey" ]] ; then
+            printf "%bQuitting from the program\n" "$titlecol3"
+            printf "%bThank You for using program!\n" "$titlecol3"
             #break
             exit 0
     fi
 }
+## Program Structure
+pName1="zoom"
+pLink1="Link to go"
+
+pName2="telega"
+
+pName3="opera"
+
+PROGLIST=($pName1 $pName2 $pName3)
+
+
 programList(){
-    printf "Choose Program"
-    
+    printf "Choose Program\n\n"
+    printf "Programs List:\n" 
+for index in ${!PROGLIST[*]}
+do
+    printf "%4d: %s\n" $index ${PROGLIST[$index]}
+done
+    printf "or press %s button to exit\n" $buttonKey
 }
 
-switcher=true
+
+switcher=false
 
 
 
@@ -37,11 +63,9 @@ fi
 
 case $keystroke in
 1)
-echo "text" && keystroke=0 && export switcher=false;;
+echo $pName1 && keystroke=0 && export switcher=false;
 esac
 
 programTerminator
+
 done
-
-
-
